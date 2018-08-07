8302f317e75d3c33f4e3c13bc9fec58868a00675
Amy Schremp <amyschremp@live.com>
2018-08-07 13:38:31 -0500

    created modal to add new journal entry

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ModalController, NavController } from 'ionic-angular';
+import { ModalPage } from '../modal/modal';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navCtrl: NavController, private modal: ModalController) {
 
+  }
+
+  openModal() {
+    const myModal = this.modal.create('ModalPage');
+
+    myModal.present();
   }
 
 }
