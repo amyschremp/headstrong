fe02a4e3020a543ff8a1eccadadd07558f8d7a3f
Amy Schremp <amyschremp@live.com>
2018-08-21 14:38:48 -0500

    closes #4

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-})
+app.use(cors())
+
+response.set('Access-Control-Allow-Origin', 'https://headstrong.app')
 
 app.use("/", express.static("www"))
 
