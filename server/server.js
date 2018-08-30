52dba346e727b8481742fd690d625f891bb25000
Amy Schremp <amyschremp@live.com>
2018-08-29 23:15:02 -0500

    user signup, salt/hash password functionality

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 10
 const cors = require('cors')
 require('dotenv').config()
 
@@ -21,6 +23,34 @@ const entrySchema = new mongoose.Schema({
 })
 
 const Entry = mongoose.model('Entry', entrySchema)
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    email: {type: String, unique: true},
+    password: String
+})
+
+userSchema.pre('save', function (next) {
+    // Pass in the scope
+    var user = this
+
+    bcrypt.hash(user.password, SALT_WORK_FACTOR, function(err, hash) {
+        if (err) return next(err)
+
+        user.password = hash
+
+        next()
+    })
+}) 
+
+userSchema.methods.comparePassword = (candidatePassword, cb) => {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+        if (err) return cb(err)
+        cb(null, isMatch)
+    }) 
+}
+
+const User = mongoose.model('User', userSchema)
 
 // app.get('/', (req, res) => res.send('Hello, world!'))
 
@@ -48,6 +78,11 @@ app.post('/api/entries/delete', (req, res) => {
         if (error) return res.sendStatus(500).json(error)
         return res.json(data)
     })
+})
+
+app.post('/api/users/add', (req, res) => {
+    let user = new User(req.body)
+    user.save((err, user) => { return err ? res.sendStatus(500).json(err) : res.json(user) })
 })
 
 app.listen(3000, () => console.log('Server running on port 3000.'))
